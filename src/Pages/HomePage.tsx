12724afdc9b421e1a769cdd74fc3b5e2c85ed780
Vladyslava Chaika <vladyslava.chaika@nure.ua>
2023-03-17 23:08:37 +0200

    Add google singin

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -9,6 +9,8 @@ import { AppDispatch } from "../store/store";
 import { selectStatus, selectAll, fetchData } from "../store/characters";
 import { useCharacter } from "../hooks/useCharacter";
 
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 
 export default function HomePage() {
   const [search, setSearch] = useState('')
@@ -24,8 +26,59 @@ export default function HomePage() {
   }, [status, dispatch])
 
 
+  /////////////////////////////////////////
+
+  const [ user, setUser ] = useState({access_token: ''});
+  const [ profile, setProfile ] = useState({picture: "", email: '', name: ''});
+
+  const login = useGoogleLogin({
+      onSuccess: (codeResponse) => setUser(codeResponse),
+      onError: (error) => console.log('Login Failed:', error)
+  });
+
+  useEffect(
+    () => {
+        if (user) {
+            axios
+                .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                    headers: {
+                        Authorization: `Bearer ${user.access_token}`,
+                        Accept: 'application/json'
+                    }
+                })
+                .then((res) => {
+                    setProfile(res.data);
+                })
+                .catch((err) => console.log(err));
+        }
+    },
+    [ user ]
+);
+
+// log out function to log the user out of google and set the profile array to null
+const logOut = () => {
+    googleLogout();
+    setProfile({picture: "", email: '', name: ''});
+};
+
+////////////////////////////////////////////////////////////////
   return (
     <div>
+      {profile.name != '' ? (
+                <div>
+                    <img src={profile.picture} alt="user" />
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <br />
+                    <br />
+                    <button onClick={logOut}>Log out</button>
+                </div>
+            ) : (
+                <button onClick={() => login()}>Sign in with Google 🚀 </button>
+            )}
+
+ ///////////////////////////////////////////////////////////////////////////////           
       <Link to={`/login`}>LOGIN</Link>
       <img src="RickMortyLogo.svg" alt="Rick and Morty Logo"/>
       <SearchField setSearch={setSearch} search={search}/>
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,19 @@ import { Provider } from 'react-redux';
 
 import Wrapper from './components/Wrapper/Wrapper';
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import { store } from './store/store';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element | DocumentFragment);
 root.render(
-  <Provider store={store}>
+  <GoogleOAuthProvider clientId='169414532568-umumk09am51487bg19cuo0pacphrj8bt.apps.googleusercontent.com'>
+   <Provider store={store}>
     <Wrapper />
-  </Provider>    
+  </Provider>      
+  </GoogleOAuthProvider>
+ 
 
 );
